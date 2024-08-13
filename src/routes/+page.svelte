2e252ac8be642e0ccd0dcfa3eef837594b4f2471
Heike Leitte <leitte@rptu.de>
2024-08-13 15:20:23 +0200

    add table and chart

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,17 +1,47 @@
 <script>
+    import Chart from "./Chart.svelte";
+    import Table from "./Table.svelte";
+    import Papa from "papaparse";
+    import * as d3 from 'd3';
+    import Scatterplot from "./Scatterplot.svelte";
+
     let files;
+    let data;
+    let xAttribute = "bill_length_mm";
+    let yAttribute = "bill_depth_mm";
+
+    const data2 = d3.ticks(-2,2,200).map(Math.sin);
+
+    $: if(files) {
+        Papa.parse(files[0], {
+            header: true,
+            dynamicTyping: true,
+            complete: function(results) {
+                data = results.data;
+            },
+            error: function(error) {
+                console.error('Error parsing CSV:', error.message);
+            }
+        });
+    }
 </script>
 
 <h1>Welcome to SvelteKit</h1>
 
 <label for="fileupload">Upload data:</label>
-<input bind:files id="fileupload" type="file"/>
+<input bind:files id="fileupload" type="file" accept=".csv"/>
 
 {#if files}
     <h2>Got file</h2>
     {#each Array.from(files) as file}
-		<p>{file.name} ({file.size} bytes)</p>
+		<div>{file.name} ({file.size} bytes)</div>
 	{/each}
+{/if}
+
+{#if data}
+    <Scatterplot data={data} x={xAttribute} y={yAttribute}/>
+    
+    <Table data={JSON.stringify(data, null, 2)} />
 {/if}
 
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
