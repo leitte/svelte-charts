cf20248af53a1ee305a3d51dd20ae5f5e3cdd6f0
Heike Leitte <leitte@rptu.de>
2024-08-13 09:35:54 +0200

    add file upload

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,9 +1,15 @@
 <script>
-    
+    let datafile;
 </script>
 
 <h1>Welcome to SvelteKit</h1>
 
+<label for="fileupload">Upload data:</label>
+<input bind:datafile id="fileupload" type="file"/>
 
+{#if file}
+    <h2>Got file</h2>
+    {file.name}
+{/if}
 
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
